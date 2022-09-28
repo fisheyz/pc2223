0a3279fe3ff5b9b1f702f05f001d25cb3af9b578
Tiago Mateus <tiago@fyde.com>
2022-09-28 10:41:17 +0100

    Add solution for UVA 11286

---
+++ UVA11286.py
@@ -0,0 +1,31 @@
+import sys
+from collections import defaultdict
+
+# was under the impression Sets had order, they dont!!!! so I believe there is no need to use one
+
+solution = []
+popularity = defaultdict(lambda: int(0))
+# classesset = set()
+t = sys.stdin.readline()
+
+while int(t) != 0:
+    for i in range(int(t)):
+        froshclasses = sys.stdin.readline().split()
+        # classesset = sorted(set([int(x) for x in froshclasses]))
+        classesset = sorted([int(x) for x in froshclasses])
+        print("classes", classesset)
+        popularity[str(classesset)] += 1
+
+    maxval = max(popularity.values())
+    maxcounter = len([k for (k, v) in popularity.items() if v == maxval])
+
+    if maxval == 1:
+        maxval = sum(popularity.values())
+    else:
+        maxval = maxval*maxcounter
+
+    solution.append(maxval)
+    popularity.clear()
+    t = sys.stdin.readline()
+
+print(*solution, sep='\n')
--- cf1077c.cr
+++ cf1077c.cr
@@ -1,48 +1,32 @@
-require "benchmark"
+# require "benchmark"
 
+# time = Benchmark.measure {
 MAXSIZE = 10**6
 
-
-# STDIN.raw do |stdin|
-#     stdin.each_line do |line|
-#       puts line
-#     end
-#   end
-
-
 t = STDIN.gets.to_s.to_i
-a = Array(Int128).new()
-STDIN.gets.to_s.split.map{|x| a.push(x.to_i)}
+a = Array(Int128).new
+sum = uninitialized Int128
 
-time = Benchmark.measure{
-    sum = uninitialized Int128
-    sum = a.sum
-    count = Array(Int32).new(MAXSIZE+1, 0)
-    solution = Array(Int32).new()
+STDIN.gets.to_s.split.map { |x| a.push(x.to_i) }
 
-    a.each_index {|i| count[a[i]]+=1}
-    i=0
-    while i <= a.size-1
-        sum = sum - a[i]
-        count[a[i]]-=1
-        if sum.even? && sum/2 <=MAXSIZE && count[sum//2] > 0
-            solution.push(i+1)
-        end
-        sum+=a[i]
-        count[a[i]]+=1
-        i+=1
-    end
-    # for i in 0..a.length-1
-    #     sum = sum - a[i]
-    #     count[a[i]]-=1
-    #     if sum.even? && sum/2 <=MAXSIZE && count[sum/2] > 0
-    #         solution.push(i+1)
-    #     end
-    #     sum+=a[i]
-    #     count[a[i]]+=1
-    # end
+sum = a.sum
+count = Array(Int32).new(MAXSIZE + 1, 0)
+solution = Array(Int32).new
 
-    p solution.size
-    solution.map{|x| print "#{x} "}
-}
+a.each_index { |i| count[a[i]] += 1 }
+i = 0
+while i <= a.size - 1
+  sum = sum - a[i]
+  count[a[i]] -= 1
+  if sum.even? && sum/2 <= MAXSIZE && count[sum//2] > 0
+    solution.push(i + 1)
+  end
+  sum += a[i]
+  count[a[i]] += 1
+  i += 1
+end
+
+p solution.size
+solution.map { |x| print "#{x} " }
+# }
 p time
