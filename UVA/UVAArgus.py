48f15f1b31f232e0bda210c857eed2c0588b2486
Tiago Mateus <tiago@fyde.com>
2022-10-05 18:07:39 +0100

    Add solution to UVA Argus

--- UVA/UVAArgus.py
+++ UVA/UVAArgus.py
@@ -1,16 +1,28 @@
-import sys
+import heapq
 
-t = sys.stdin.readline()
-# TODO
-#   Create priority queue
-#   Iterate on the input untill you find '#' and add t to the queue
+q = []
+solution = []
+
 #   for _ in range(nr of queries):
 #       pop the highest priority elements
 #       increment priority(period) of each element
 #       add them on the queue again
 #
-while str(t) != '#':
 
-    t = sys.stdin.readline()
-t = sys.stdin.readline()
-print("output")
+t = input()
+while t != '#':
+    line = t.split()
+    q.append((int(line[2]), int(line[1]), int(line[2])))
+    heapq.heapify(q)
+    t = input()
+
+# let there be k queries to do and since the time complexity of the heapreplace method is log(n)
+# the overall time complexity on the program is klog(n)
+#
+# https://stackoverflow.com/questions/33701160/python-heapq-difference-between-heappushpop-and-heapreplace
+for i in range(1, int(input())+1):
+    tmp = q[0]
+    solution.append(tmp[1])
+    heapq.heapreplace(q, (tmp[0]+tmp[2], tmp[1], tmp[2]))
+
+print(*solution, sep='\n')
